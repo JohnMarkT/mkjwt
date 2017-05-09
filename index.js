3fb00e0d50ec669e7cb04a30a4e69222fd117398
JohnMarkT <github@johnmarkthrasher.com>
2017-05-09 16:25:14 -0400

    Add index.js

---
+++ index.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+/*jshint node:true, esversion:6 */
+const program = require('commander');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const pkgjson = require('./package.json');
+
+const version = pkgjson.version.replace("+", " - ");
+
+program
+  .version(version)
+  .description(pkgjson.description)
+  .usage('[options]')
+  .option('-p, --payload [file]', 'JSON file containing payload', 'payload.json')
+  .option('-s, --secret <string>', 'Secret string with single quotes', 'secret')
+  .option('-e, --expires', 'Expiration for token', '30d')
+  .option('-v, --verbose', 'Show details')
+  .parse(process.argv);
+
+const jwtOptions = {
+        "algorithm": "HS256",
+        "expiresIn": program.expires
+    };
+
+fs.readFile(program.payload, 'utf8', (err, data) => {
+    if (err) {
+        if (err.code === 'ENOENT') {
+            handleError(`${program.payload} not found`);
+        }
+        handleError(err);
+    }
+    const payload = safeParse(data);
+    if (!payload) {
+        handleError("Please provide a valid JSON file for the payload");
+    }
+    createJWT(payload);
+});
+
+function createJWT(payload) {
+    jwt.sign(payload, program.secret, jwtOptions, (err, token) => {
+        if (err) {
+            handleError(err);
+        }
+        
+        if (program.verbose) {
+            console.log("SECRET:", program.secret);
+            console.log("PAYLOAD:", JSON.stringify(payload, null, 4));
+            console.log("JWT:", token);
+        } else {
+            console.log(token);
+        }
+        process.exit(0);
+    });
+}
+
+function handleError(...msgs) {
+    msgs.forEach(msg => console.error(msg));
+    program.help();
+}
+
+function safeParse(content){
+  try {
+    return JSON.parse(content);
+  } catch(ex){
+    return '';
+  }
+}
